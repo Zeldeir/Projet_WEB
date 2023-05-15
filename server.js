507fc32da2b82f7b45b24b86a147ed160fc8bbce
vraux <user.name@esiea.fr>
2023-05-15 15:27:02 +0200

    start_project

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-test2
+var express = require('express');
+var serve_static = require('serve-static');
+var http = require('http');
+
+var app = express();
+
+app.use(serve_static(__dirname+"/public"));
+
+console.log(__dirname + "/public")
+
+var serveur = http.Server(app);
+
+var io = require('socket.io').listen(serveur);
+
+io.sockets.on('connection', function(socket){
+
+    console.log("Un client s'est connecté!");
+    
+    socket.on('disconnect', function(){
+        console.log("Un client s'est déconnecté!");
+    })
+})
+
+//Ecoute sur un seul port
+serveur.listen(8080, function(){
+    console.log("Serveur en écoute sur le port 8080");
+});
